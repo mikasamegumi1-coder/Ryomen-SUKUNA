c9184ca2a5fd47209ac247a859029878319bf7a5
mikasamegumi1-coder <mikasamegumi1@gmail.com>
2026-07-21 18:42:00 +0700

    Add initial HTML content to app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.getElementById("app").innerHTML = `
+<h1 style="padding:40px;text-align:center;">
+👑 Ryomen Sukuna
+</h1>
+
+<p style="text-align:center;">
+King of Curses Analytics
+</p>
+`;
